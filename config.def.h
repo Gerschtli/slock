823c296a1538e8a41a9de744fb743e519c6a3f84
Tobias Happ <tobias.happ@gmx.de>
2017-07-25 21:02:03 +0200

    Copies config.h to config.def.h

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#3077a0",   /* after initialization */
+	[INPUT] =  "#2c7f1f",   /* during input */
+	[FAILED] = "#330606",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
